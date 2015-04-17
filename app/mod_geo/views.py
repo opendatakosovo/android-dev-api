f83808ebefa3cbd9309bab9a4590117db43b225e
Grammos <kamberigramos@gmail.com>
2015-04-17 15:12:02 +0200

    Create a POST request function. When invoked, it saves the request's JSON data payload as a document in MongoDB.

--- app/mod_geo/views.py
+++ app/mod_geo/views.py
@@ -1,12 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Response, request
 from flask.views import View
-
+from bson import json_util
 from app import mongo 
 
-mod_geo = Blueprint('geo', __name__)
-@mod_geo.route('/')
+mod_geo = Blueprint('geo', __name__, url_prefix='/')
+
+@mod_geo.route('', methods=['GET'])
+def home():
+	'''
+	Show home page
+	'''
+	return render_template('mod_geo/geo.html')
+
+
+@mod_geo.route('geo-api', methods=['POST'])
 def geo():
-	'''
-	Show geos page!
-	'''
-	return render_template('mod_geo/index.html')
+	json_string = request.data
+	json_obj = json_util.loads(json_string)
+	print request.data
+	mongo.db.geodata.insert(json_obj)
+	
+	resp = Response(status=200, mimetype='application/json')
+
+	return resp
